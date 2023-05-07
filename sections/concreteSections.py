3685a7c2aab2f3e84c8e1c10161f848110d6b269
mahdi thi <mhitarahomy@gmail.com>
2023-05-07 23:17:02 +0430

    Delete all functions for RebarCoords

--- sections/concreteSections.py
+++ sections/concreteSections.py
@@ -1,19 +1,17 @@
 from dataclasses import dataclass, field
-from typing import Any, TypedDict, Optional, List, Tuple
+from typing import Any, List, Tuple
 from enum import StrEnum, auto
 from math import sin, cos, pi
-from matplotlib import pyplot as plt
 
-from shapely import Point, Polygon, LineString
-from shapely.affinity import rotate, translate
-from shapely.plotting import plot_line, plot_points, plot_polygon
+from shapely import Point, Polygon
+from shapely.affinity import translate
 
-from pyCivil.materials import ConcreteMat, RebarMat, AIII, AII, C25
-from pyCivil.sections.rebarSections import Rebar, GRebars
+from ..materials import ConcreteMat, RebarMat, AIII, AII, C25
 
 
 class ListOfPoints(List[Tuple[float, float]]):
     ...
+
 
 class SectionType(StrEnum):
     Triangle = auto()
@@ -29,27 +27,10 @@ class SectionType(StrEnum):
     Custom = auto()
 
 
-class ConfType(StrEnum):
-    Tie = auto()
-    Spiral = auto()
-
-
-@dataclass
-class Cover():
-    Top: float
-    Bottom: float
-    Right: float
-    Left: float
-    
-
-@dataclass
-class RebarCoords():
-    point: tuple[float, float]
-    rebar: Rebar|GRebars
-
 C25def = lambda: C25
 AIIdef = lambda: AII
 AIIIdef = lambda: AIII
+
 
 @dataclass()
 class ConcreteSct():
@@ -58,24 +39,11 @@ class ConcreteSct():
     concMat: ConcreteMat
     lBarMat: RebarMat 
     cBarMat: RebarMat
-    confType: ConfType
-    rebarCoords: list[RebarCoords] = field(init=False, default_factory=list)
     
     @property
     def sectionCoords(self) -> ListOfPoints:
         return ListOfPoints(list(Polygon(self.section).exterior.coords))
-    @property
-    def rebarNums(self) -> int: return len(self.rebarCoords)
-
-    @property
-    def coords(self) -> list[tuple[float, float]]: return [self.rebarCoords[i].point for i in range(self.rebarNums)]
     
-    @property
-    def rebarList(self) -> list[Rebar|GRebars]: return [self.rebarCoords[i].rebar for i in range(self.rebarNums)]
-
-    @property
-    def As(self) -> list[float]: return [self.rebarList[i].Area for i in range(self.rebarNums)]
-
 
 @dataclass()
 class RectConcreteSct(ConcreteSct):
@@ -86,26 +54,12 @@ class RectConcreteSct(ConcreteSct):
     sectionType: SectionType = field(init=False, default=SectionType.Rectangle)
     lBarMat: RebarMat = field(default_factory=AIIIdef)
     cBarMat: RebarMat = field(default_factory=AIIdef)
-    confType: ConfType = field(default=ConfType.Tie)
-    rebarCoords: list[RebarCoords] = field(init=False, default_factory=list)
 
     def __setattr__(self, __name: str, __value: Any) -> None:
         super().__setattr__(__name, __value)
         if __name=="b" or __name=="h":
             self.section =  RectangleSct(self.b, self.h)
     
-    @property
-    def TopEdge(self): return ListOfPoints([self.sectionCoords[0], self.sectionCoords[1]])
-    
-    @property
-    def BottomEdge(self): return ListOfPoints([self.sectionCoords[2], self.sectionCoords[3]])
-    
-    @property
-    def LeftEdge(self): return ListOfPoints([self.sectionCoords[1], self.sectionCoords[2]])
-    
-    @property
-    def RightEdge(self): return ListOfPoints([self.sectionCoords[3], self.sectionCoords[0]])
-
 
 # region Create Sections
 def moveCentroidToOrigin(section: Polygon) -> Polygon:
@@ -173,96 +127,16 @@ def CreateEllipseSct(a: float, b: float) -> list[tuple[float, float]]:
     return list(sct.exterior.coords)
 # endregion
 
-# region Create Rebar Shapes
-def QuadrilateralRebarShape(xNum: int, yNum: int, rebarSct: Rebar|GRebars) -> list[list[Rebar|GRebars]]:
-    return [[rebarSct for i in range(xNum)] if (j == 0 or j == yNum-1) else [rebarSct, rebarSct] for j in range(yNum)]
 
-
-def LinearRebarShape(num: int, rebarSct:Rebar|GRebars) -> list[Rebar|GRebars]:
-    return [rebarSct for i in range(num)]
-
-
-def CircularRebarShape(num: int, rebarSct:Rebar|GRebars) -> list[Rebar|GRebars]:
-    return [rebarSct for i in range(num)]
-# endregion
-
-# region Create & Edit Rebars
-def setCover(cover:Cover|float|int):
-    return cover if type(cover)==Cover else Cover(cover, cover, cover, cover) if (
-        type(cover)==float or type(cover)==int) else Cover(0, 0, 0, 0)
-
-def PointRebar(rebar: Rebar|GRebars, point: tuple[float, float]) -> list[RebarCoords]:
-    return [RebarCoords(point=point, rebar=rebar)]
-
-def LinearRebars(barShape: list[Rebar|GRebars], startCover:float, endCover:float , line: ListOfPoints, 
-                 offsetDist: float=0, offsetSide: str = "left", 
-                 barDistances: list[float]|None = None) -> list[RebarCoords]:
-    barsNum = len(barShape)
-    if (barDistances != None) and (len(barDistances) != len(barShape)):
-        raise ValueError("barShape and barDistances must have same length.")
-    _line = LineString(line)
-    pStart = _line.parallel_offset(offsetDist, offsetSide).interpolate(startCover)
-    pEnd = _line.parallel_offset(offsetDist, offsetSide).interpolate(_line.length-endCover)
-    d = ([i / (barsNum-1) for i in range(barsNum)] if barsNum!=1 else [0.5]) if barDistances == None else barDistances
-    return [RebarCoords(point=list(LineString([pStart, pEnd]).interpolate(d[i], normalized=True).coords)[0], rebar=barShape[i]) for i in range(barsNum)]
-
-def QuadrilateralRebars(barShape: list[list[Rebar|GRebars]], cover: Cover|float|int, 
-                              points: ListOfPoints, barDistances: list[list[float]]|None=None,
-                              layerDistances: list[float]|None=None) -> list[RebarCoords]:
-    _cover = setCover(cover)
-    barsNumList: list[int] = [len(s) for s in barShape]
-    if 5<len(points)<4: raise ValueError("Number of points must be 4")
-    p00, p11, p22, p33 = Point(points[0]), Point(points[1]), Point(points[2]), Point(points[3])
-    line0 = LineString([p00, p33])
-    line1 = LineString([p11, p22])
-    p0 = line0.interpolate(_cover.Top)
-    p3 = line0.interpolate(line0.length-_cover.Bottom)
-    p1 = line1.interpolate(_cover.Top)
-    p2 = line1.interpolate(line1.length-_cover.Bottom)
-    d = ([i / (len(barsNumList)-1) for i in range(len(barsNumList))]
-         if len(barsNumList) != 1 else [0.5]) if layerDistances == None else layerDistances
-    rebarCoords = []
-    for i in range(len(barsNumList)):
-        pStart: Point = LineString([p0, p3]).interpolate(d[i], normalized=True)
-        pEnd: Point = LineString([p1, p2]).interpolate(d[i], normalized=True)
-        if  barDistances == None:
-            _rebarCoords = LinearRebars(barShape[i], _cover.Right, _cover.Left, [pStart.coords[0], pEnd.coords[0]])  # type: ignore
-        else: 
-            _rebarCoords = LinearRebars(barShape[i], _cover.Right, _cover.Left, [pStart.coords[0], pEnd.coords[0]], barDistances=barDistances[i]) # type: ignore
-        rebarCoords.extend(_rebarCoords)
-    return rebarCoords
-
-def RectSectRebars(section: ListOfPoints, xNum: int, yNum: int, rebar: Rebar|GRebars, cover: Cover|float|int):
-    rshape = QuadrilateralRebarShape(xNum, yNum, rebar)
-    return QuadrilateralRebars(rshape, cover, section)
-
-def CircularBars(barShape: list[Rebar|GRebars], cover: float, D: float) -> list[RebarCoords]:
-    barsNum = len(barShape)
-    r = (D/2) - cover
-    alphas = [i * (360/barsNum) for i in range(barsNum)]
-    return [RebarCoords(rebar=barShape[i], point=rotate(Point(0, r), alphas[i], Point(0, 0))) for i in range(barsNum)]
-
-def DeleteRebars(rebarCoords: list[RebarCoords], *args: int) -> list[RebarCoords]:
-    output = rebarCoords
-    for index in sorted(args, reverse=True):
-            del output[index]
-    return output
-
-def ChangeRebars(rebarCoords: list[RebarCoords], changeList: list[tuple[int, Rebar|GRebars]]):
-    output = rebarCoords
-    for item in changeList:
-            output[item[0]].rebar = item[1]
-    return output
-
-def showSection(concSct: ConcreteSct) -> None:
-    fig = plt.figure(dpi=90)
-    ax = fig.add_subplot()  # type: ignore
-    plot_polygon(Polygon(concSct.section), add_points=False, linewidth=1)
-    if concSct.rebarCoords != None:
-        plot_points([Point(rcoord.point) for rcoord in concSct.rebarCoords], color='black')
-        for i in range(len(concSct.rebarCoords)):
-            rcoord = concSct.rebarCoords[i]
-            ax.annotate(f"{rcoord.rebar}\n{i}", xy=(rcoord.point[0], rcoord.point[1]), xycoords='data',
-                    xytext=(1.5, -10), textcoords='offset points')
-    plt.show()
-# endregion
+# this code must go to concreteAnalysis
+# def showSection(concSct: ConcreteSct) -> None:
+#     fig = plt.figure(dpi=90)
+#     ax = fig.add_subplot()  # type: ignore
+#     plot_polygon(Polygon(concSct.section), add_points=False, linewidth=1)
+#     if concSct.rebarCoords != None:
+#         plot_points([Point(rcoord.point) for rcoord in concSct.rebarCoords], color='black')
+#         for i in range(len(concSct.rebarCoords)):
+#             rcoord = concSct.rebarCoords[i]
+#             ax.annotate(f"{rcoord.rebar}\n{i}", xy=(rcoord.point[0], rcoord.point[1]), xycoords='data',
+#                     xytext=(1.5, -10), textcoords='offset points')
+#     plt.show()
