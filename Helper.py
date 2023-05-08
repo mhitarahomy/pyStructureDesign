4212489a751c4ba61af3ebe491c2e3c8edd24aa4
Tarahomi <mhitarahomy@gmail.com>
2023-05-08 12:50:29 +0430

    add for test  packge modules

--- Helper.py
+++ Helper.py
@@ -1,5 +1,5 @@
-from shapely import Point
- from pyCivilDesign.
+from shapely import Point, Polygon
+from pyCivilDesign.sections.section import ListOfPoints
 
-# li = [(1,2), (3,4), (5,6), (7,8)]
-# print(ListOfPoints(li))
+li = [(1,2), (3,4), (5,6), (7,8)]
+print(ListOfPoints(Polygon(li).exterior.coords))
