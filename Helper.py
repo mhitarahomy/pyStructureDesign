d8685b6cc30db8bdf0d4622fba71260b5bfaf17c
Tarahomi <mhitarahomy@gmail.com>
2023-05-08 16:49:25 +0430

    use for test

--- Helper.py
+++ Helper.py
@@ -1,5 +1,5 @@
 from shapely import Point, Polygon
 from pyCivilDesign.sections.section import ListOfPoints
+import math
 
-li = [(1,2), (3,4), (5,6), (7,8)]
-print(ListOfPoints(Polygon(li).exterior.coords))
+a = 5
