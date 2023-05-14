fb9a852108153ffe21a7af9817702cb87b6bf956
Tarahomi <mhitarahomy@gmail.com>
2023-05-14 16:37:16 +0430

    test functions

--- Helper.py
+++ Helper.py
@@ -10,14 +10,18 @@ from shapely import LineString, Point, Polygon
 from shapely.plotting import plot_line, plot_points, plot_polygon
 
 
-ListOfPoints = List[Tuple[float, float]]
+B300x400 = RectConcreteSct(b=300, h=400)
+B300x400.rebarCoords = Rsct.RectSectRebars(B300x400.section, 4, 2, Rsct.d20, 50)
+print(B300x400.bw)
+showSection(B300x400)
+# ListOfPoints = List[Tuple[float, float]]
 
-section = SCT.TrapzoidSct(b1=400, b2=600, h=700)
+# section = SCT.TrapzoidSct(b1=400, b2=600, h=700)
 
-print(section)
-fig = plt.figure(dpi=90)
-ax = fig.add_subplot()
-plot_polygon(Polygon(section))
-plot_line(LineString(SCT.Edge(section, "left")), add_points=False, color="red")
-# plot_points(endPoint)
-plt.show()
+# print(section)
+# fig = plt.figure(dpi=90)
+# ax = fig.add_subplot()
+# plot_polygon(Polygon(section))
+# plot_line(LineString(SCT.Edge(section, "left")), add_points=False, color="red")
+# # plot_points(endPoint)
+# plt.show()
