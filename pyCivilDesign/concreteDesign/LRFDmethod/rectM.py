76f89f5baa00b7fec499f0f13bc8f09b2d5971d4
Tarahomi <mhitarahomy@gmail.com>
2023-05-22 14:36:40 +0430

    rearrange

---
+++ pyCivilDesign/concreteDesign/LRFDmethod/rectM.py
@@ -0,0 +1,7 @@
+
+# TODO: Rectangle section M3 desgin functions must be here
+
+from pyCivilDesign.concreteDesign.designProps import DesignData, Assumptions, defaultAssumption
+
+def Calcd(): # TODO: var d must calculated 
+    pass
