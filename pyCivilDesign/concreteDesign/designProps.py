603b130ad372b7b7a7d0b47f9b8505cf6ad44143
Tarahomi <mhitarahomy@gmail.com>
2023-05-22 14:11:14 +0430

    remove assumptions

--- pyCivilDesign/concreteDesign/designProps.py
+++ pyCivilDesign/concreteDesign/designProps.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import Callable
 from pyCivilDesign.sections.concreteSections import ConcreteSct
 import numpy as np
 from numpy.typing import NDArray
@@ -21,30 +20,6 @@ class DesignData():
     @classmethod
     def fromSection(cls, section: ConcreteSct):
         return setDesignDataFromSection(section)
-
-
-beta1: Callable[[DesignData], np.float32] = lambda data: \
-    np.float32(0.85) if data.fc<=28 else np.max([0.85-(0.05*(data.fc-28)/7), np.float32(0.65)])
-
-
-def phif(data: DesignData, esMin: np.float32) -> np.float32:
-    ety = data.fy / data.Es
-    et = abs(esMin)
-    return np.float32(0.65) if et <= ety else 0.65+0.25*((et-ety)/0.003)\
-          if ety < et < ety+0.003 else np.float32(0.9)
-
-
-@dataclass()
-class Assumptions():
-    beta1: Callable[[DesignData], np.float32] = beta1
-    phif: Callable[[DesignData, np.float32], np.float32] = phif
-    phis: np.float32 = np.float32(0.9)
-    phia: np.float32 = np.float32(0.65)
-    phit: np.float32 = np.float32(0.6)
-    ecu: np.float32 = np.float32(0.003)
-
-
-defaultAssumption = Assumptions()
 
 
 def setDesignDataFromSection(sct: ConcreteSct) -> DesignData:
