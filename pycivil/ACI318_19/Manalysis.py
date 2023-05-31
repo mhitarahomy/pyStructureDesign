1e65cdf56816441e48c56095f22a7d149d0d44e9
Tarahomi <mhitarahomy@gmail.com>
2023-05-31 14:19:52 +0430

    add positive and negative moments

--- pycivil/ACI318_19/Manalysis.py
+++ pycivil/ACI318_19/Manalysis.py
@@ -84,15 +84,26 @@ def calc_Fsz(data: DesignData, c: float) -> np.float32:
     return np.sum(Fs * yCoords)
 
 
-def calc_M(data: DesignData, c: float, IsPhi: bool = True) -> np.float32:
+def calc_M(data: DesignData, c: float, IsPhi: bool = True) -> Tuple[np.float32, np.float32]:
     Fszy = calc_Fsz(data, c)
     Cc = calc_Cc(data, c)
     pressure_region = calc_pressure_region(data.section, data.fc, c)
     zcy = abs(pressure_region.centroid.y)
     es = calc_es(data.section, data.Coords, c)
     phi = assump.phif(data.fy, data.Es, min(es))
-    Mx = phi*(Cc*zcy + abs(Fszy)) if IsPhi else Cc*zcy + abs(Fszy)
-    return Mx
+    M_pos = phi*(Cc*zcy + abs(Fszy)) if IsPhi else Cc*zcy + abs(Fszy)
+    
+    data_180 = data
+    data_180.section = rotate_section(data_180.section, 180)
+    data_180.Coords = rotate_rebar_coords(data_180.Coords, 180)
+    Fszy = calc_Fsz(data_180, c)
+    Cc = calc_Cc(data_180, c)
+    pressure_region = calc_pressure_region(data_180.section, data_180.fc, c)
+    zcy = abs(pressure_region.centroid.y)
+    es = calc_es(data_180.section, data_180.Coords, c)
+    phi = assump.phif(data_180.fy, data_180.Es, min(es))
+    M_neg = phi*(Cc*zcy + abs(Fszy)) if IsPhi else Cc*zcy + abs(Fszy)
+    return M_pos, M_neg
 
 
 calc_P = partial(PMM.calc_P, angle=0)
@@ -113,19 +124,21 @@ def calc_c(data: DesignData) -> float:
     return root_scalar(_optim_c, bracket=[0.0001, c_max]).root
 
 
-def calc_Mn(data: DesignData) -> np.float32:
+def calc_Mn(data: DesignData) -> Tuple[np.float32, np.float32]:
     c = calc_c(data)
     return calc_M(data, c)
 
 
 def calc_M_ratio(data: DesignData, Mux: float) -> np.float32:
-    return Mux/calc_Mn(data)
+    Mn_pos, Mn_neg = calc_Mn(data)
+    Mn = Mn_pos if Mux>=0 else Mn_neg
+    return Mux/Mn
 
-
-def calc_percent(data: DesignData, Mux: float) -> np.float32:
+# ! min and max reinforcement must add
+def calc_percent(data: DesignData, Mux: float) -> np.float32: 
     def _optim_percent(x):
         data_percent = set_As_percent(data, x)
-        return calc_Mn(data_percent) - Mux
+        return calc_M_ratio(data_percent, Mux) - 1
     data_one_percent = set_As_percent(data, 1)
     data_eight_percent = set_As_percent(data, 8)
     if calc_M_ratio(data_one_percent, Mux) <= 1: 
